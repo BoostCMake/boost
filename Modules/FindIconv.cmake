ec0fc026a7bb7495026427f0b61bdb9d295208b7
Mikhail Komarov <nemo1369@gmail.com>
2016-12-02 02:44:59 +0300

    Fixed Boost.Locale crosscompile build by removing path restrictions in FindIconv.cmake module. Fixed CMake modules path.

--- Modules/FindIconv.cmake
+++ Modules/FindIconv.cmake
@@ -1,52 +1,50 @@
-# - Try to find Iconv 
-# Once done this will define 
-# 
-#  ICONV_FOUND - system has Iconv 
-#  ICONV_INCLUDE_DIR - the Iconv include directory 
-#  ICONV_LIBRARIES - Link these to use Iconv 
+# - Try to find Iconv
+# Once done this will define
+#
+#  ICONV_FOUND - system has Iconv
+#  ICONV_INCLUDE_DIR - the Iconv include directory
+#  ICONV_LIBRARIES - Link these to use Iconv
 #  ICONV_SECOND_ARGUMENT_IS_CONST - the second argument for iconv() is const
-# 
+#
 include(CheckCCompilerFlag)
 include(CheckCSourceCompiles)
 
-IF (ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
-  # Already in cache, be silent
-  SET(ICONV_FIND_QUIETLY TRUE)
-ENDIF (ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
+if(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
+    # Already in cache, be silent
+    set(ICONV_FIND_QUIETLY TRUE)
+endif(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
 
-IF(APPLE)
-    FIND_PATH(ICONV_INCLUDE_DIR iconv.h
-             PATHS
-             /opt/local/include/
-             NO_CMAKE_SYSTEM_PATH
-    )
+if(APPLE)
+    find_path(ICONV_INCLUDE_DIR iconv.h
+            PATHS
+            /opt/local/include/
+            )
 
-    FIND_LIBRARY(ICONV_LIBRARIES NAMES iconv libiconv c
-             PATHS
-             /opt/local/lib/
-             NO_CMAKE_SYSTEM_PATH
-    )
-ENDIF(APPLE)
+    find_library(ICONV_LIBRARIES NAMES iconv libiconv c
+            PATHS
+            /opt/local/lib/
+            )
+endif(APPLE)
 
-FIND_PATH(ICONV_INCLUDE_DIR iconv.h PATHS /opt/local/include /sw/include)
+find_path(ICONV_INCLUDE_DIR iconv.h PATHS /opt/local/include /sw/include)
 
 string(REGEX REPLACE "(.*)/include/?" "\\1" ICONV_INCLUDE_BASE_DIR "${ICONV_INCLUDE_DIR}")
 
-FIND_LIBRARY(ICONV_LIBRARIES NAMES iconv libiconv c HINTS "${ICONV_INCLUDE_BASE_DIR}/lib" PATHS /opt/local/lib)
- 
-IF(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES) 
-   SET(ICONV_FOUND TRUE) 
-ENDIF(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES) 
+find_library(ICONV_LIBRARIES NAMES iconv libiconv c HINTS "${ICONV_INCLUDE_BASE_DIR}/lib" PATHS /opt/local/lib)
+
+if(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
+    set(ICONV_FOUND TRUE)
+endif(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
 
 set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
 set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
-IF(ICONV_FOUND)
-  check_c_compiler_flag("-Werror" ICONV_HAVE_WERROR)
-  set (CMAKE_C_FLAGS_BACKUP "${CMAKE_C_FLAGS}")
-  if(ICONV_HAVE_WERROR)
-    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")
-  endif(ICONV_HAVE_WERROR)
-  check_c_source_compiles("
+if(ICONV_FOUND)
+    check_c_compiler_flag("-Werror" ICONV_HAVE_WERROR)
+    set(CMAKE_C_FLAGS_BACKUP "${CMAKE_C_FLAGS}")
+    if(ICONV_HAVE_WERROR)
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")
+    endif(ICONV_HAVE_WERROR)
+    check_c_source_compiles("
   #include <iconv.h>
   int main(){
     iconv_t conv = 0;
@@ -57,24 +55,24 @@ IF(ICONV_FOUND)
     iconv(conv, &in, &ilen, &out, &olen);
     return 0;
   }
-" ICONV_SECOND_ARGUMENT_IS_CONST )
-  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS_BACKUP}")
-ENDIF(ICONV_FOUND)
+" ICONV_SECOND_ARGUMENT_IS_CONST)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_BACKUP}")
+endif(ICONV_FOUND)
 set(CMAKE_REQUIRED_INCLUDES)
 set(CMAKE_REQUIRED_LIBRARIES)
 
-IF(ICONV_FOUND) 
-  IF(NOT ICONV_FIND_QUIETLY) 
-    MESSAGE(STATUS "Found Iconv: ${ICONV_LIBRARIES}") 
-  ENDIF(NOT ICONV_FIND_QUIETLY) 
-ELSE(ICONV_FOUND) 
-  IF(Iconv_FIND_REQUIRED) 
-    MESSAGE(FATAL_ERROR "Could not find Iconv") 
-  ENDIF(Iconv_FIND_REQUIRED) 
-ENDIF(ICONV_FOUND) 
+if(ICONV_FOUND)
+    if(NOT ICONV_FIND_QUIETLY)
+        message(STATUS "Found Iconv: ${ICONV_LIBRARIES}")
+    endif(NOT ICONV_FIND_QUIETLY)
+else(ICONV_FOUND)
+    if(Iconv_FIND_REQUIRED)
+        message(FATAL_ERROR "Could not find Iconv")
+    endif(Iconv_FIND_REQUIRED)
+endif(ICONV_FOUND)
 
-MARK_AS_ADVANCED(
-  ICONV_INCLUDE_DIR
-  ICONV_LIBRARIES
-  ICONV_SECOND_ARGUMENT_IS_CONST
+mark_as_advanced(
+        ICONV_INCLUDE_DIR
+        ICONV_LIBRARIES
+        ICONV_SECOND_ARGUMENT_IS_CONST
 )
